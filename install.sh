40d397a153baf537b9ebfdc7930f8727e3da3cb8
Burke <rburketaylor@gmail.com>
2015-07-06 02:55:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ wget --output-document="/etc/default/deluge-daemon" https://raw.githubuserconten
 wget --output-document="/etc/init.d/deluge-daemon" https://raw.githubusercontent.com/iamtis/deluge-installer/master/etc/init.d/deluge-daemon
 chmod a+x /etc/init.d/deluge-daemon
 update-rc.d deluge-daemon defaults
-sudo rm /var/www/html/index.html
+rm /var/www/html/index.html
 chmod 777 /var/www/html
 reboot -h now
